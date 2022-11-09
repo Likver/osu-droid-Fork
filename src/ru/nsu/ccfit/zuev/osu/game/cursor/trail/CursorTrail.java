9459a605da84aab1c8bde36a4ca60d9e9c475ca2
Likver <anonymatebg@gmail.com>
2022-11-09 00:23:59 +0000

    vscode commit 2

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/trail/CursorTrail.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/trail/CursorTrail.java
@@ -25,7 +25,6 @@ public class CursorTrail extends ParticleSystem {
             Config.isUseLongTrail()? trailAmount * longTrailRateMultiplier : trailAmount,
             Config.isUseLongTrail()? trailAmount * longTrailRateMultiplier : trailAmount,
             Config.isUseLongTrail()? trailAmount * longTrailMaxMultiplier : trailAmount,
-            Config.isUseLongTrail()? trailAmount * longTrailMaxMultiplier : trailAmount,
             pTextureRegion
         );
 
@@ -41,6 +40,6 @@ public class CursorTrail extends ParticleSystem {
         float lifeTime = Config.isUseLongTrail()? longLifeTime : defaultLifeTime;
 
         addParticleModifier(new ExpireModifier(lifeTime));
-        addParticleModifier(new AlphaModifier(1.0f, 0.0f, 0.0f, 0f, lifeTime));
+        addParticleModifier(new AlphaModifier(1.0f, 0.0f, 0f, lifeTime));
     }
 }
